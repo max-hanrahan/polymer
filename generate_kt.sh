dd65def5b824ce5d020f57379ce6a8860a6e8c0d
Max Hanrahan <mhanrahan@wesleyan.edu>
2022-11-06 22:22:16 -0500

    k1/t0.8 is now a test dir

--- generate_kt.sh
+++ generate_kt.sh
@@ -1,19 +1,22 @@
 PVAR=0.0
 NVAR=8000
 FVAR=5
+TSTEP=0.3
 
 # read user input
 KVAR=$1
 TVAR=$2
 
+T_HIGH="$(echo "$TVAR + $TSTEP" | bc)"
+
 printf "Pressure: $PVAR\n"
 printf "Num. Atoms: $NVAR\n"
-printf "Stiffened Chains: $FVAR\n" todo: check with Starr to make sure this is right
+printf "Stiffened Chains: $FVAR percent \n"
 
 # if needed, make the specific directories
 mkdir -p k${KVAR}; mkdir -p k${KVAR}/t${TVAR}
-# copy t1.2 restart to this new directory:
-cp k${KVAR}/t1.2/k${KVAR}t1.2p0.0f5n8000npt-1.res k${KVAR}/t${TVAR}/
+# copy t${TVAR+TSTEP} restart to this new directory:
+cp k${KVAR}/t${T_HIGH}/k${KVAR}t${T_HIGH}p${PVAR}f${FVAR}n${NVAR}npt-1.res k${KVAR}/t${TVAR}/
 
 cp -r kkk/ttt/. k${KVAR}/t${TVAR}/ # copy abstract directory contents into specific dir
 cd k${KVAR}/t${TVAR}/
