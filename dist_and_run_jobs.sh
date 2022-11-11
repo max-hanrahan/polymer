80ffdf2523addbc6426905d40d4d0e84e4196fb3
Max Hanrahan <mhanrahan@wesleyan.edu>
2022-11-11 10:17:32 -0500

    ran all with poly group instead of stiffs, k10t0.8 now a test dir

---
+++ dist_and_run_jobs.sh
@@ -0,0 +1,17 @@
+printf "What is your desired temperature?\n"
+read TVAR
+
+printf "What is the minimum available node?"
+read minnode
+
+printf "Which nodes to skip? (enter numbers)"
+read -a arr
+
+# for (int i = 0; i < 7; i++) {
+#     do int j = minnode + i
+#     # pseudocode
+#     if j in arr
+#         do j++;
+#     # replace to the n54
+#
+# }
